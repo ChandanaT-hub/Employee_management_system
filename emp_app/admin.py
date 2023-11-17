e613f67feb237760eac8207daf7b79fe762b6f21
unknown <chandanaraj2001@gmail.com>
2023-11-17 23:28:02 +0530

    first commit

---
+++ emp_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from.models import employee,role,departments
+# Register your models here.
+admin.site.register(employee)
+admin.site.register(role)
+admin.site.register(departments)
