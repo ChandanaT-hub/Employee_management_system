7dce2311c13bbd1cac4667a7f9a1ff2493998fea
Chandana <chandanaraj2001@gmail.com>
2023-11-22 19:21:38 +0530

    first commit

--- book_app/views.py
+++ book_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from.models import book
+from django.db.models import Q
 def index(request):
     return render(request, 'wel.html')
 
@@ -19,7 +20,7 @@ def add_book(request):
         cost = int(request.POST['cost'])
         new_book=book(b_name=b_name,a_name=a_name,cost=cost)
         new_book.save()
-        return HttpResponse('Employee added successfully')
+        return HttpResponse('BOOK added successfully')
     elif request.method == "GET":
         return render(request,"add.html")
     else:
@@ -27,7 +28,35 @@ def add_book(request):
 
 
 def remove_book(request):
-    return HttpResponse("EMPLOYEE REMOVED")
+    if request.method == "POST":
+        b_name = request.POST['b_name']
+        b1 = book.objects.all()
+        if b_name:
+            b1 = b1.filter(b_name__icontains=b_name)
+            b1.delete()
+        return HttpResponse("BOOK REMOVED")
+        return render(request, "view.html")
+    b1 = book.objects.all()
+    context = {
+        'b1': b1
+    }
+    return render(request, "remove.html", context)
 
 def filter_book(request):
-    return render(request,'filter.html')
+    if request.method=="POST":
+        b_name=request.POST['b_name']
+        a_name = request.POST['a_name']
+        b1=book.objects.all()
+        if b_name:
+            b1=b1.filter(Q(b_name__icontains=b_name))
+        if a_name:
+            b1 = b1.filter(Q(a_name__icontains=a_name))
+        contents={
+            'b1':b1
+        }
+        return render(request,'view.html',contents)
+    elif request.method=="GET":
+        return render(request,"filter.html")
+    else:
+        return HttpResponse("INVALID INFORMATION")
+
